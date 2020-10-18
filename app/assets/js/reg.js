85b5ed6c2dc9e70f88db6deeb02f8ee1d274d37e
lovaszbotond <botond.lovasz@gmail.com>
2020-10-18 16:54:16 +0200

    Regisztrációs felület animációjának feltöltése - js kód - Botond

---
+++ app/assets/js/reg.js
@@ -0,0 +1,73 @@
+// input anim
+const labels = document.querySelectorAll('.regisztracio-form-control label');
+
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+        .split('').map((letter, idx) =>
+            `<span style="transition-delay: ${idx * 50}ms">${letter}</span>`).join('');
+});
+
+//Regiszt Anim
+const logo = document.querySelectorAll("#regisztracioAnimacio path");
+
+for (let i = 0; i < logo.length; i++) {
+    console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
+}
+//Regiszt anim vége
+
+//Profil feltöltés
+//4 hivatkozas a html elementekre -> 1 - inpFile 2 - div image preview 3 - img tag 4 - span tag
+
+const inpFile = document.getElementById("inpFile");
+const previewContainer = document.getElementById("imagePreview");
+const previewImage = previewContainer.querySelector(".image-preview__image");
+const previewDefaultText = previewContainer.querySelector(".image-preview__default-text");
+
+//listener
+inpFile.addEventListener("change", function() {
+    const file = this.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+
+        previewDefaultText.style.display = "none";
+        previewImage.style.display = "block";
+
+        reader.addEventListener("load", function() {
+            previewImage.setAttribute("src", this.result);
+        });
+        reader.readAsDataURL(file);
+    } else {
+        previewDefaultText.style.display = null;
+        previewImage.style.display = null;
+        previewImage.setAttribute("src", "");
+    }
+});
+
+//Profil feltöltés vége
+
+//bankkartyaszam input checkbox 
+
+const bankszam = document.querySelector('#bankszam');
+const szovegBox = document.querySelectorAll('.elofizet');
+
+var checkBox = document.getElementById('elofizetek');
+var checkBoxx = document.getElementById('nemfizet');
+
+bankszam.style.display = "none";
+
+checkBox.addEventListener('click', function() {
+
+    bankszam.style.display = "block";
+
+    const tl = new TimelineMax({ delay: 0 });
+
+    tl.fromTo(szovegBox, 0.4, { y: "50" }, { y: "0" })
+        .fromTo(bankszam, 0.7, { x: '-350%' }, { x: '0%' });
+
+
+})
+
+checkBoxx.addEventListener('click', function() {
+    bankszam.style.display = "none";
+})
