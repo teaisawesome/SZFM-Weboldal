ef6917958f9c506276228207522acf199665e03b
lovaszbotond <botond.lovasz@gmail.com>
2020-11-14 19:25:58 +0100

    nav template hozzáadása

---
+++ app/view/template/navtemp.php
@@ -0,0 +1,26 @@
+<header class="borito">
+        <div class="borito-img">
+            <nav>
+                <div class="topmenu">
+                    <div class="logo">Logo</div>
+                    <label for="btn" class="icon">
+                        <span class="fa fa-bars"></span>
+                    </label>
+                    <input class="csekk" type="checkbox" id="btn">
+                    <ul>
+                        <li><a href="">Forum</a></li>
+                        <li><a href="">Galeria</a></li>
+                        <li><a href="">Kapcsolat</a></li>
+                        <li><a href="">Szolgaltatasok</a></li>
+                        <li><a href="">Rolunk</a></li>
+                        <li><a href="">Belepes</a></li>
+                        <li><a href="">Regisztracio</a></li>
+                    </ul>
+                </div>
+            </nav>
+            <div class="cim">
+                <h1>Szauna</h1>
+                <h3>Salutem</h3>
+            </div>
+        </div>
+    </header>
