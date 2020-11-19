9f6466622cb78a77c900efd4b6a3d4ddfdc59115
lovaszbotond <botond.lovasz@gmail.com>
2020-11-19 19:03:38 +0100

    teknos.html js anim/effekt/slider feltöltése

---
+++ app/assets/js/szolgaltatas_oldalak_js/teknos.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    $('.post-wrapper').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        nextArrow: $('.next'),
+        prevArrow: $('.prev'),
+        responsive: [{
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 1000,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
