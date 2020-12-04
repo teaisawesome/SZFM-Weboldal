fe146f0771951488058509728b546151aa478543
Róbert Dékány <dekany.r123@gmail.com>
2020-12-04 01:47:04 +0100

    profile design done

--- app/view/profile.php
+++ app/view/profile.php
@@ -1,3 +1,10 @@
+<?php 
+  if(isset($_POST['exit']))
+  {
+    session_destroy();
+    header("Location: http://localhost/SZFM-Weboldal/app/");
+  }
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -17,15 +24,60 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12 img-section">
-                <img src="datas/pictures/<?php 
+                <img id="profile-img" src="datas/pictures/<?php 
                     echo $this->user->getImage();               
                 ?>" alt="" width="150" height="150">
             </div>
-            <div class="col-md12">
-               <h3>Név: <?php echo $this->user->getName(); ?></h3>
-               asd
+        </div>
+        <div class="row profile-row">
+            <div class="col-md-6">
+              <div class="profile-info-section">
+                <h5><strong>Név:</strong> <?php echo $this->user->getName(); ?></h5>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="profile-info-section">
+                <h5><strong>Email:</strong> <?php echo $this->user->getEmail(); ?></h5>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="profile-info-section">
+                <h5><strong>Lakcím:</strong> <?php echo $this->user->getAddress(); ?></h5>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="profile-info-section">
+                <h5><strong>Kártya:</strong> <?php 
+                $card = $this->user->getCard();
+                
+                if($card)
+                {
+                  echo $card;
+                }
+                else
+                {
+                  echo "Nincs megadva.";
+                }
+                
+                ?></h5>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="profile-info-section">
+                <h5><strong>Telefonszám:</strong> <?php echo $this->user->getPhone(); ?></h5>
+              </div>
             </div>
         </div>
+        <form action="" method="post">
+        <div class="row profile-row text-center p-5">
+            <div class="col-md-6 profile-button">
+                <button class="btn btn-warning">Adataim szerkesztése</button>
+            </div>
+            <div class="col-md-6 profile-button">
+                <input class="btn btn-info" name="exit" type="submit" value="Kilépés">
+            </div>
+        </div>
+        </form>
     </div>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
