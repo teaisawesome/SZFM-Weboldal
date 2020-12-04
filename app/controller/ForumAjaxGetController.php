637c17325a59381b45458785f0613f53b69a5aaf
petrusjt <jozsefpetrus99@gmail.com>
2020-12-04 21:30:37 +0100

    Fórum kicsit szebbé téve

--- app/controller/ForumAjaxGetController.php
+++ app/controller/ForumAjaxGetController.php
@@ -4,6 +4,7 @@ class ForumAjaxGetController extends Controller
 {
     public function index()
     {
+        require_once ROOT . "view/template/navtemp.php";
         require_once ROOT . "ajax/forum_ajax.php";
     }
 }
--- app/view/forum.php
+++ app/view/forum.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SZFM - Fórum</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="assets/css/navtemp.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
     <link rel="stylesheet" type="text/css" href="assets/css/forum.css">
 </head>
 <body>
